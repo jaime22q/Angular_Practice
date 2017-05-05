a12225b524bc21f6af32ab989e620064f44e2dcf
Jaime Quintero <jaimeq22@Jaimes-MacBook-Pro.local>
2017-05-05 00:24:07 -0400

    changes for adding new servers

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -10,6 +10,7 @@ export class ServersComponent implements OnInit {
   serverCreationSt = `No server was created`;
   serverName = '';
   serverCreated = false;
+  servers = ['TestServer1','TestServer2'];
 
   constructor() {
   setTimeout(()=> {
@@ -20,6 +21,7 @@ export class ServersComponent implements OnInit {
   ngOnInit() {
   }
 onCreatedServer(){
+  this.servers.push(this.serverName);
   this.serverCreated = true;
   this.serverCreationSt = `Server was created! The server name is ${this.serverName}`;
 }
