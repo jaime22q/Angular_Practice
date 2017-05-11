aa9a40102e2fed39b4dc702032924bcc2a9c0cf4
Jaime Quintero <jaimeq22@Jaimes-MacBook-Pro.local>
2017-05-11 00:19:34 -0400

    latest changes on prac-directives component

--- src/app/prac-directives/prac-directives.component.ts
+++ src/app/prac-directives/prac-directives.component.ts
@@ -6,13 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./prac-directives.component.css']
 })
 export class PracDirectivesComponent implements OnInit {
- cnt = 0;
+cnt = 0;
+visible = true;
+repetition = [];
+paragraph = "This paragraph will toggle with the button.";
+
+
   toggle(){
+  this.paragraph = "This paragraph will toggle with the button.";
   this.cnt++;
-  
-}
-display(){
-  return true;
+  this.repetition.push(this.cnt);
+  this.cnt % 2 == 0 ? this.visible = true : this.visible = false;
+  // console.log(this.cnt);
 }
   constructor() { }
 
